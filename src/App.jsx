648b028ea11b1625954e1af9548a2a9136f68158
nsirtak <nsirtak@gmail.com>
2024-11-03 09:17:50 -0600

    indent code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 
-const todoList = [
-  { id: 1, title: "Complete week 1 assignment" },
-  { id: 2, title: "Go shopping" },
-  { id: 3, title: "Go to gym" }
-];
-
-function App() {
-  return (
-    <div>
+  const todoList = [
+      { id: 1, title: "Complete week 1 assignment" },
+      { id: 2, title: "Go shopping" },
+      { id: 3, title: "Go to the gym" }
+    ];
+    function App() {
+    return (
+      <div>
       <h1>Todo List</h1>
       <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
+      {todoList.map((todo) => (
+      <li key={todo.id}>{todo.title} </li>
+
+      ))}
+
       </ul>
     </div>
-  );
-}
-
-export default App;
+    )
+  }
+  export default App
