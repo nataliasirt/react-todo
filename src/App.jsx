87c8e72b3e8482ea3eb665857ee9122f7dfcdcc8
nsirtak <nsirtak@gmail.com>
2025-01-06 11:53:39 -0600

    week9-10

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,69 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
-  const todoList = [
-      { id: 1, title: "Complete week 1 assignment" },
-      { id: 2, title: "Go shopping" },
-      { id: 3, title: "Go to the gym" }
-    ];
-    function App() {
-    return (
-      <div>
+function App() {
+  const [todoList, setTodoList] = useState([]); // default state is empty array
+  const [isLoading, setIsLoading] = useState(true); // loading state
+
+  const fetchData = async () => {
+    console.log("Fetching data...");
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      },
+    };
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+    console.log("URL:", url);
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("Fetched Data:", data);
+
+      const todos = data.records.map((record) => ({
+        id: record.id,
+        title: record.fields.title,
+      }));
+      console.log("Todos from Airtable:", todos);
+
+      setTodoList(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching todos:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const addTodo = (newTodo) => {
+    setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
+  };
+
+  const removeTodo = (id) => {
+    setTodoList((prevList) => prevList.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div>
       <h1>Todo List</h1>
-      <ul>
-      {todoList.map((todo) => (
-      <li key={todo.id}>{todo.title} </li>
-
-      ))}
-
-      </ul>
+      {isLoading ? (
+        <p>Loading...</p> // conditional loading indicator
+      ) : (
+        <>
+          <AddTodoForm onAddTodo={addTodo} />
+          <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+        </>
+      )}
     </div>
-    )
-  }
-  export default App
+  );
+}
+
+export default App;
---
+++ src/InputWithLabel.jsx
@@ -0,0 +1,23 @@
+import { useRef, useEffect } from "react";
+
+const InputWithLabel = ({ label, value, onChange }) => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus(); // Focus on the input element
+  }, []);
+
+  return (
+    <>
+      <label>{label}</label>
+      <input
+        ref={inputRef}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </>
+  );
+};
+
+export default InputWithLabel;
